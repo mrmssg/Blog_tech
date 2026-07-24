ac9bc3e4cafb1bc89f3e2c1382a9f3c09ee18748
Mauro S S Goncalves <133293974+mrmssg@users.noreply.github.com>
2026-07-24 19:06:37 +0000

    add buttone script

---
+++ main.js
@@ -0,0 +1,6 @@
+const botao = document.querySelector("button");
+botao.addEventListener("click", botaoClicado);
+function botaoClicado() {
+let texto = botao.querySelector("span");
+texto.textContent++;
+}
